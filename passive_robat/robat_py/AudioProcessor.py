fixed time sleep after output and speed up the computation

passive_robat/robat_py/AudioProcessor.py
@@ -25,12 +25,9 @@ import datetime
 import time
 import random
 import os
-import threading
 import pandas as pd 
 import netifaces as ni
 import time
-
-
 
 # Stream callback function
 class AudioProcessor:
@@ -230,7 +227,6 @@ class AudioProcessor:
             return avar_theta, dB_SPL_level
 
     def update_das(self):
-
         start_time = time.time()
         in_buffer = self.buffer
         # print('buffer queue time seconds=', start_time_1 - start_time)
@@ -280,7 +276,7 @@ class AudioProcessor:
 
         max_envelope_idx = np.argmax(filtered_envelope)
         max_envelope_value = filtered_envelope[max_envelope_idx]
-        print('Max envelope value:', max_envelope_value)
+        # print('Max envelope value:', max_envelope_value)
 
         # Trim a 10 ms part around the max
         trim_ms = 10
@@ -306,7 +302,7 @@ class AudioProcessor:
 
 
         start_time_3 = time.time()
-        print('matched filter time seconds=', start_time_3 - start_time_2)
+        # print('matched filter time seconds=', start_time_3 - start_time_2)
 
         # Plot matched filtered signal and detected peaks for the reference channel
         # plt.figure(figsize=(10, 4))
@@ -344,7 +340,7 @@ class AudioProcessor:
         freqwise_Parms = freqrms/self.interp_sensitivity
         
         start_time_4 = time.time()
-        print('rms freqwise time =', start_time_4 - start_time_3)
+        # print('rms freqwise time =', start_time_4 - start_time_3)
         # # Calculate and save the average noise spectrum (ANS) figure
 
         # plt.figure(figsize=(10, 4))
@@ -368,7 +364,7 @@ class AudioProcessor:
         dB_SPL_level = pascal_to_dbspl(total_rms_freqwise_Parms) #dB SPL level for reference channel
         print('db SPL:', dB_SPL_level)
 
-        print('time to calculate dB SPL =', time.time() - start_time_4)
+        # print('time to calculate dB SPL =', time.time() - start_time_4)
 
         start_time_5 = time.time()
         theta, spatial_resp, f_spec_axis, spectrum, bands = das_filter(trimmed_signal, self.fs, self.channels, self.mic_spacing, [self.highpass_freq, self.lowpass_freq], theta=self.theta_das)
@@ -385,7 +381,7 @@ class AudioProcessor:
         # plt.close()
 
         start_time_6 = time.time()
-        print('das computation =', start_time_6 - start_time_5)
+        # print('das computation =', start_time_6 - start_time_5)
 
         #spatial_resp = gaussian_filter1d(spatial_resp, sigma=4)
         peaks, _ = signal.find_peaks(spatial_resp)  # Adjust height as needed
@@ -398,14 +394,13 @@ class AudioProcessor:
         top_n_peak_indices = np.argsort(peak_heights)[-N:]  # Indices of the N largest peaks # Indices of the N largest peaks
         top_n_peak_indices = top_n_peak_indices[::-1]
         peak_angles = theta[peaks[top_n_peak_indices]]  # Corresponding angles
-        print('peak angles', peak_angles, 'peak heights', peak_heights[top_n_peak_indices])
+        print('peak angles', peak_angles, 'peak heights', peak_heights[top_n_peak_indices], '\n')
 
         end_time = time.time()
 
-        print('peak finding =', end_time - start_time_6)
+        # print('peak finding =', end_time - start_time_6)
 
-        print('update time seconds =', end_time - start_time)
-
+        # print('update time seconds =', end_time - start_time)
         if dB_SPL_level > self.trigger_level or dB_SPL_level > self.critical_level:
             return peak_angles[0], dB_SPL_level
         else:

passive_robat/robat_py/functions/das_v2.py
@@ -46,7 +46,7 @@ def das_filter(y, fs, nch, d, bw, theta, c=343, wlen=64, show=False):
   
   time2 = time.time()
   
-  print('STFT computation time:', time2 - time1)
+  # print('STFT computation time:', time2 - time1)
 
   # speed version of DAS 
   band_idxs = np.where((f_spec_axis >= bw[0]) & (f_spec_axis <= bw[1]))[0]
@@ -66,7 +66,7 @@ def das_filter(y, fs, nch, d, bw, theta, c=343, wlen=64, show=False):
 
       if show:
           ax.plot(theta_rad, 10 * np.log10(np.abs(p_i)), label=f'{bands[b_idx]:.1f} Hz')
-  print('DAS computation time 1 :', time.time() - time2)
+  # print('DAS computation time 1 :', time.time() - time2)
   mag_p = np.abs(p)/len(bands)
 
   # # normal version 

passive_robat/robat_py/latency.py
@@ -27,7 +27,7 @@ fs = 48000
 t_tone = np.linspace(0, tone_durn, int(fs*tone_durn))
 chirp = signal.chirp(t_tone, 20e3, t_tone[-1], 2e2)
 chirp *= signal.windows.hann(chirp.size)
-output_chirp = np.concatenate((chirp, np.zeros((int(fs*0.02)))))
+output_chirp = np.concatenate((chirp, np.zeros((int(fs*0.005)))))
 
 plt.figure(figsize=(10, 6))
 t_audio = np.linspace(0, output_chirp.shape[0]/fs, output_chirp.shape[0])
@@ -70,7 +70,7 @@ all_input_data = queue.Queue() # Creates a queue object to store input data
 start_time = time.time()
 try:    
     with stream_inout:
-        while (time.time() - start_time) < 0.5:
+        while (time.time() - start_time) < 0.15:
             pass
     stream_inout.stop()
 except :

passive_robat/robat_py/robatv1_2.py
@@ -26,7 +26,6 @@ import threading
 import pandas as pd 
 import netifaces as ni
 
-
 from thymiodirect import Connection 
 from thymiodirect import Thymio
 
@@ -40,9 +39,7 @@ from functions.detect_peaks import detect_peaks
 from AudioProcessor import AudioProcessor
 from RobotMove import RobotMove
 from shared_queues import angle_queue, level_queue
-
 # Create queues for storing data
-shared_audio_queue = queue.Queue()
 recording_bool = False  # Set to True to record audio, False to just process audio without recording
 print('imports done')
 
@@ -56,13 +53,13 @@ raspi_local_ip = ni.ifaddresses('wlan0')[2][0]['addr']
 print('raspi_local_ip =', raspi_local_ip)
 
 # Parameters for the DOA algorithms
-trigger_level =  60 # dB SPL
-critical_level = 65 # dB SPL
+trigger_level =  70 # dB SPL
+critical_level = 77 # dB SPL
 c = 343   # speed of sound
 fs = 48000
 
 rec_samplerate = 48000
-input_buffer_time = 0.1 # seconds
+input_buffer_time = 0.04 # seconds
 block_size = int(input_buffer_time*fs)  #used for the shared queue from which the doa is computed, not anymore for the output stream
 channels = 5
 mic_spacing = 0.018 #m
@@ -84,12 +81,12 @@ echo = pra.linear_2D_array(center=[(channels-1)*mic_spacing//2,0], M=channels, p
 
 # Parameters for the DAS algorithm
 theta_das = np.linspace(-90, 90, 61) # angles resolution for DAS spectrum
-N_peaks = 2 # Number of peaks to detect in DAS spectrum
+N_peaks = 1 # Number of peaks to detect in DAS spectrum
 
 # Parameters for the chirp signal
 duration_out = 10e-3  # Duration in seconds
 silence_pre = 0 # [ms] can probably pushed to 20 
-silence_post = 60 # [ms] can probably pushed to 20
+silence_post = 110 # [ms] can probably pushed to 20
 amplitude = 0.5 # Amplitude of the chirp
 
 t = np.linspace(0, duration_out, int(fs*duration_out))
@@ -254,12 +251,13 @@ if __name__ == '__main__':
 
     move_thread = threading.Thread(target=robot_move.audio_move, daemon = True)
     move_thread.start()
-    event = threading.Event()
-
+    event = threading.Event() 
+    
     try:
         while True:
+            start_time = time.time() 
             event.clear()
-            start_time = time.time()      
+            # print('start_time =', start_time)  
             with sd.OutputStream(samplerate=fs,
                                 blocksize=0, 
                                 device=usb_fireface_index, 
@@ -267,37 +265,40 @@ if __name__ == '__main__':
                                 callback=audio_processor.callback_out, finished_callback=event.set, latency='low') as out_stream:
                                     with out_stream:
                                         event.wait()
-                                        print('event time =', time.time() - start_time)
+                                        
+                                        # print('event time =', time.time() - start_time)
                                         if method == 'CC':
                                             time.sleep(input_buffer_time*2.3)
-            print('out time =', time.time() - start_time)  
-            time.sleep(0.25)                     
+                                    
+
+            # print('out time =', time.time() - start_time)  
+            # time.sleep(0.25)                     
             start_time_1 = time.time()
               # Allow some time for the audio input to be processed
             if method == 'CC':
-                  # Adjust this sleep time as needed
-                # print('0 time =', time.time() - start_time_1) 
                 args.angle, dB_SPL_level = audio_processor.update()  
                 angle_queue.put(args.angle)
                 level_queue.put(dB_SPL_level)
-
 
                 if isinstance(args.angle, (int, float, np.number)):
                     if np.isnan(args.angle):
                         angle_queue.put(None)
 
             elif method == 'DAS':
-
                 args.angle, dB_SPL_level = audio_processor.update_das()
+
+                # print(time.time() - start_time_1, 'DAS time')
+                # print(time.time() , 'end time')
                 angle_queue.put(args.angle)
                 level_queue.put(dB_SPL_level)
+
 
             else:
                 print('No valid method provided')
                 robot_move.stop()
 
 
-            print('in time =', time.time() - start_time_1)
+            # print('in time =', time.time() - start_time_1)
         else:
             robot_move.stop()
 